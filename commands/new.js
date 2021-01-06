6e99b5264d8ed6648a26e4497e7c225e5591c182
DadmehrX <73789666+DadmehrX@users.noreply.github.com>
2021-01-06 15:50:56 +0330

    Update new.js

--- commands/new.js
+++ commands/new.js
@@ -1,12 +1,12 @@
- const Discord = require("discord.js");
+const Discord = require("discord.js");
 const config = require("./config.json");
 let client = new Discord.Client(),
 
 module.exports = {
-  name: "play",
+  name: "new",
   cooldown: 3,
-  aliases: ["p"],
-  description: "Plays audio from YouTube or Soundcloud",
+  aliases: [""],
+  description: "Newmember Comming",
 
 
 
@@ -30,28 +30,6 @@ client.on("message", message => {
   }
 });
 
-
-
-var used1 = false;
-client.on('ready', () => {
-    console.log('On ShoD ');
-    client.user.setStatus('online').catch(console.error);
-    setInterval(() => {
-      let membersCount = client.guilds.cache.map(guild => guild.memberCount).reduce((a, b) => a + b, 0)
-      var guild = client.guilds.cache.get('724360597038301199')
-      var onlineCount = guild.members.cache.filter(m => m.presence.status === 'online').size 
-        if(used1){
-          client.user.setActivity(`تعداد کاربران : ${membersCount}`,{
-          type: 'LISTENING'
-/*           url: 'https://www.twitch.tv/XXX', */
-          });
-        used1 = false;}
-      else{
-         client.user.setActivity("Welcome To Divinity=))",{
-        type: "LISTENING"
-        });
-        used1 = true;}
-    }, 25000);
 
 });
 
